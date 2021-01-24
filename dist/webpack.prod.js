e8e97d0f73d4473e56300224d4b0df133ea0a1f4
Milena Kari <milenakari@yahoo.com>
2021-01-24 11:45:54 -1000

    Add dev and production webpack config scripts

---
+++ dist/webpack.prod.js
@@ -0,0 +1,34 @@
+const path = require('path') 
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'production',
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.png$/,
+                loader: 'file-loader'
+            },
+
+    ]   
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+        
+
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
